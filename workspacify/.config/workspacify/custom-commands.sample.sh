72b0ad0f85a763faeeb4af6af3e354c845d653f7
Matej <martes@zloprcek.localdomain>
2023-06-24 12:03:47 +0200

    update: general overhaul

--- workspacify/.config/workspacify/custom-commands.sample.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/sh
-
-declare -A custom_commands
-
-custom_commands[project] = './scripts/run_project.sh' 
-
-# App will fallback to this when no docker-compose is found
-# so make sure to add here apps you are not starting with docker-compose

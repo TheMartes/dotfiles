61271b7ca55a7cdf08796032cc3ec95d18ac4c2b
TheMartes <hello@themartes.com>
2025-01-23 21:49:43 +0100

    treesiter bump

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -7,7 +7,7 @@ local M = {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "query", "vim", "typescript", "javascript", "html", "css", "tsx", "php", "go", "rust", "zig" },
+          ensure_installed = { "c", "lua", "query", "vim", "typescript", "javascript", "html", "css", "tsx", "php", "go", "rust", "zig", "nix", "odin", "hocon", "ini", "json" },
           sync_install = false,
           highlight = { enable = true, additional_vim_regex_highlighting = false },
           indent = { enable = true },
